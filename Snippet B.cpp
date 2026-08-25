b5a009f758aed11fa28dca9a3ca29551d4f27c0f
cruzishell-ui <cruzishell@gmail.com>
2026-08-24 18:12:14 -0600

    Add files via upload

---
+++ Snippet B.cpp
@@ -0,0 +1,14 @@
+/* Snippet B */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int FirstScore = 95;
+    double class_avg = 88.5;
+    const double PI = 3.14159;
+    
+    char studentGrade = 'A'; 
+
+    cout << "First Score: " << FirstScore << endl;
+    return 0;
+}
